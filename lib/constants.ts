f55ab8f195187df7e9b0fd140eb7d2dff246da4f
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-02-22 23:13:31 +0000

    fix CMS_NAME

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const EXAMPLE_PATH = 'blog-starter'
+export const CMS_NAME = 'Markdown'
 export const HOME_OG_IMAGE_URL =
   'https://cdn.iconscout.com/icon/free/png-256/code-1767550-1502454.png'
