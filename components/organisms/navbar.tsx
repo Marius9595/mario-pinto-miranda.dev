84f597378561437a513cb7cc7b0fe450542c2b27
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2023-02-28 22:02:40 +0000

    Personalized navbar

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -1,36 +1,82 @@
-import Link from 'next/link';
-
+import React from "react";
 import {
   Box,
   Flex,
-  Text,
-  Button,
-  useColorModeValue,
+  HStack,
+  Link,
+  IconButton,
+  useDisclosure,
   Stack,
-  useColorMode,
-} from '@chakra-ui/react';
+  Icon,
+  Text
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+import { MdCode } from 'react-icons/md'
 
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+const Links = ["Blog", "Timeline"];
 
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: "orange.500"
+    }}
+    href={"#"}
+  >
+    <Text>{children}</Text>
+  </Link>
+);
 
-export const Nav = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+export  function Nav() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <Box>
-            <Link href="/">
-                <Text fontWeight={'bold'}>🧑🏽‍💻 Home</Text>
+    <>
+      <Box bg={"orange.600"} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+          
+            <Link href="/" _hover={{textDecoration: "none"}}>
+            <Flex alignItems="center">
+                <Icon as={MdCode} w={8} h={8} marginEnd={2}/>
+                <Text fontSize="l" fontWeight="bold">
+                  Mario S. Pinto Miranda
+                </Text>
+              </Flex>
             </Link>
-          </Box>
-          <Flex alignItems={'center'}>
-            <Stack direction={'row'} spacing={7}>
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
-            </Stack>
-          </Flex>
+
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map(link => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
         </Flex>
+
+        {isOpen ? (
+          <Box pb={4}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map(link => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
       </Box>
+    </>
   );
 }
